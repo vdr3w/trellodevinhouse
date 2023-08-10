f3880728fe586e339b273e7896e262afaec48b43
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-10 15:00:42 -0300

    [M1S09] Ex 1-8

---
+++ semana-7/ListaMedicamentosVue/src/components/CardMedicamento.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <button @click="toggleFavorite">Favoritar</button>
+    <img src="../assets/medimg.jpg" alt="Imagem Medicamento Placeholder" />
+    <h3>{{ nome }}</h3>
+    <p>Laboratório: {{ laboratorio }}</p>
+    <p>Preço: R${{ preco }},00</p>
+    <p>Favorito: {{ favorito ? "Sim" : "Não" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardMedicamento",
+  props: {
+    id: Number,
+    nome: String,
+    laboratorio: String,
+    preco: [Number, String],
+    favorito: Boolean,
+  },
+  methods: {
+    toggleFavorite() {
+      this.$emit("favoritarMedicamento", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 10px;
+}
+
+img {
+  max-width: 120px;
+}
+</style>
---
+++ semana-7/ListaMedicamentosVue/src/components/FormularioNovoMedicamento.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <input
+        type="text"
+        v-model="medicationName"
+        placeholder="Nome do Medicamento" />
+      <input
+        type="text"
+        v-model="laboratoryName"
+        placeholder="Nome do Laboratório" />
+      <input type="number" v-model="price" placeholder="Preço" />
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormularioNovoMedicamento",
+  data() {
+    return {
+      medicationName: "",
+      laboratoryName: "",
+      price: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit("submitMedication", {
+        medicationName: this.medicationName,
+        laboratoryName: this.laboratoryName,
+        price: this.price,
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
