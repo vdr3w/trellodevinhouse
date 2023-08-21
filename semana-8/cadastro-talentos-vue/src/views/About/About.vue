b8eb0de1696caf6319352cb3f1e6717e92066f18
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-21 17:53:55 -0300

    Exercicios

--- semana-8/cadastro-talentos-vue/src/views/About/About.vue
+++ semana-8/cadastro-talentos-vue/src/views/About/About.vue
@@ -3,30 +3,47 @@
     <h1 class="title">SOBRE</h1>
     <h2 class="subtitle">QUEM SOMOS NÓS</h2>
     <p class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac
-      venenatis dolor. Nullam eget ultrices quam. Nulla facilisi. Suspendisse
-      potenti. Nulla facilisi.
+      A Empresa XYZ é líder no setor de tecnologia, fornecendo soluções
+      inovadoras para seus clientes em todo o mundo. Com uma abordagem centrada
+      no cliente, trabalhamos continuamente para superar as expectativas.
     </p>
     <p class="content">
-      Vestibulum at tincidunt orci. Nunc tincidunt ligula nec erat congue, ut
-      venenatis enim volutpat. Donec varius nec dolor sit amet imperdiet.
+      Fundada em 2000, nossa equipe de profissionais altamente qualificados está
+      empenhada em criar produtos e serviços que definam o padrão de excelência.
+      Nosso foco está em qualidade, integridade e em um atendimento ao cliente
+      incomparável.
     </p>
     <p class="content">
-      Quisque fermentum, libero at convallis facilisis, justo dolor posuere dui,
-      id tristique enim urna quis libero. Vivamus lacinia est in odio dignissim,
-      id iaculis eros laoreet.
+      Com escritórios em várias cidades ao redor do globo, a Empresa XYZ é sua
+      parceira confiável em tecnologia. Seja em software, hardware ou
+      consultoria, nós estamos aqui para ajudar você a alcançar seus objetivos
+      de negócios.
     </p>
     <p class="content">
-      Sed pellentesque, ligula vel viverra suscipit, neque arcu convallis odio,
-      a tristique enim mi vel quam. Fusce tincidunt quam sit amet massa luctus,
-      at tincidunt quam feugiat.
+      Nosso compromisso com a sustentabilidade e a responsabilidade social
+      também faz parte do nosso DNA. Investimos em práticas ecológicas e
+      apoiamos causas que refletem nossos valores e nossa visão de um futuro
+      melhor.
     </p>
     <p class="content">
-      Praesent ut massa non risus aliquet aliquam. Sed vitae tortor vel elit
-      lacinia efficitur ut eget neque. Sed quis odio eget lectus imperdiet
-      dignissim.
+      Entre em contato conosco hoje e descubra como nossas soluções podem
+      beneficiar você e sua empresa. A equipe da Empresa XYZ está ansiosa para
+      colaborar com você no seu próximo projeto de sucesso.
     </p>
   </div>
+  <br />
+  <div class="frame-mapa">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3601.4742035075346!2d-49.21541022378147!3d-25.489226935239298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dcfaa2a999a87b%3A0x6fde02b4649a371!2sR.%20Buti%C3%A1%2C%20141%20-%20Uberaba%2C%20Curitiba%20-%20PR%2C%2081560-420!5e0!3m2!1spt-BR!2sbr!4v1692650651214!5m2!1spt-BR!2sbr"
+      width="1200"
+      height="450"
+      style="border: 0"
+      allowfullscreen="true"
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+  <br />
+  <br />
 </template>
 
 <style scoped>
@@ -52,5 +69,11 @@
   margin: 20px;
   line-height: 1.5;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.frame-mapa {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
