1944ea64aee865ad391084b133e7d2984d0de1e8
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-23 22:31:34 -0300

    Update main.js

--- semana-9/atividade-andrev/src/main.js
+++ semana-9/atividade-andrev/src/main.js
@@ -3,9 +3,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from "./plugins/vuetify"; // Certifique-se de ter essa linha correta
+import vuetify from "./plugins/vuetify";
 
-createApp(App)
-  .use(router)
-  .use(vuetify) // Certifique-se de usar o Vuetify aqui
-  .mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
